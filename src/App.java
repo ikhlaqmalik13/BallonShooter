26a343cc88506031056ed9dae1786612aa7d7f69
IkhlaqMalik <ikhlaqmalik.si@gmail.com>
2023-08-23 15:21:58 +0530

    - Adde the object of BaloonShooter - ikhlaq

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import home.BallonShooter;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new BallonShooter();
     }
 }
